ddcbce327d52216ac2d019898c4c32ceb960c280
Alba Moreno <alba.moreno95@gmail.com>
2022-03-02 22:42:33 +0100

    remove non used imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import ProductsList from './ProductsList';
 import NotFoundPage from './NotFoundPage';
-import { useState, useEffect } from 'react';
-import { PaginationContext } from '../context/paginationContext';
-import { useProductsHook } from '../hooks/useProductsHook';
 
 const WebPages = () => {
-
-
     return (
         <Routes>
             <Route path="/" element={<Home />} />
