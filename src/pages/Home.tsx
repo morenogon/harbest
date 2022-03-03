aaef463f9d6184b95e8ad397ae8e39b47c1eea58
Alba Moreno <alba.moreno95@gmail.com>
2022-03-03 09:59:16 +0100

    styling Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
+import '../styles/Home.scss';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <img src='https://harbestmarket.com/wp-content/uploads/2020/06/harbest-market-1.png' alt='' />
+      <div>
+        <h2>De la huerta a tu restaurante</h2>
+        <h3 className='home__description'>Compra frutas y hortalizas directamente a agricultores sin intermediarios</h3>
+      </div>
+      <Link className="home__link" to='/products'>Explorar</Link>
+    </div>
   )
 }
 
